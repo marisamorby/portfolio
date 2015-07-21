30d219167743727952b54e0aa3e517b03621de6d
jlengstorf <jason.lengstorf@copterlabs.com>
2015-07-21 12:24:14 +0200

    feat(parallax): added parallax and info about adding localscroll

--- wp-content/themes/marisamorby/assets/js/main.js
+++ wp-content/themes/marisamorby/assets/js/main.js
@@ -12,6 +12,102 @@ jQuery(function($){
     /*
      * CUSTOM THEME JS
      *************************************************************************/
+    if (size == 'desktop') {
 
+        // Sets the background images to scroll 40% more slowly than the page.
+        $('#about>img').parallaxHw(0, -0.4);
+        $('#work>img').parallaxHw(0, -0.4);
+        $('#blog>img').parallaxHw(100, -0.4);
+        $('#contact>img').parallaxHw(150, -0.4);
+
+        // Set the headings to move 20% more slowly than the page.
+        $('.about-me-header').parallaxHw(0, -0.2);
+        $('#work>h2').parallaxHw(0, -0.2);
+        $('#blog>h2').parallaxHw(0, -0.2);
+        $('#contact>h2').parallaxHw(0, -0.2);
+
+        // If you want to add local scrolling, use this:
+        // http://flesler.blogspot.it/2007/10/jquerylocalscroll-10.html
+    }
 
 });
+
+/*
+Plugin: jQuery Parallax
+Version 1.1.3
+Author: Ian Lunn
+Twitter: @IanLunn
+Author URL: http://www.ianlunn.co.uk/
+Plugin URL: http://www.ianlunn.co.uk/plugins/jquery-parallax/
+
+Dual licensed under the MIT and GPL licenses:
+http://www.opensource.org/licenses/mit-license.php
+http://www.gnu.org/licenses/gpl.html
+*/
+
+(function( $ ){
+    var $window = $(window);
+    var windowHeight = $window.height();
+
+    $window.resize(function () {
+        windowHeight = $window.height();
+    });
+
+    $.fn.parallaxHw = function(xpos, speedFactor, outerHeight, fade) {
+        var $this = $(this);
+        var getHeight;
+        var firstTop;
+        var paddingTop = 0;
+        
+        //get the starting position of each element to have parallax applied to it      
+        $this.each(function(){
+            firstTop = $this.offset().top;
+        });
+
+        if (outerHeight) {
+            getHeight = function(jqo) {
+                return jqo.outerHeight(true);
+            };
+        } else {
+            getHeight = function(jqo) {
+                return jqo.height();
+            };
+        }
+            
+        // setup defaults if arguments aren't specified
+        if (arguments.length < 1 || xpos === null) xpos = "50%";
+        if (arguments.length < 2 || speedFactor === null) speedFactor = 0.1;
+        if (arguments.length < 3 || outerHeight === null) outerHeight = true;
+        if (arguments.length < 4 || fade === null) fade = false;
+        
+        // function to be called whenever the window is scrolled or resized
+        function update(){
+            var pos = $window.scrollTop();              
+
+            $this.each(function(){
+                var $element = $(this);
+                var top = $element.offset().top;
+                var height = getHeight($element);
+
+                // Check if totally above or totally below viewport
+                if (top + height < pos || top > pos + windowHeight) {
+                    return;
+                }
+
+                $this.css('-webkit-transform', 'translate3d(0px, ' + (Math.round((firstTop - pos) * speedFactor)) + 'px, 0px)');
+                $this.css('transform', 'translate3d(0px, ' + (Math.round((firstTop - pos) * speedFactor)) + 'px, 0px)');
+                if (fade == 'home') {
+                    $this.css('opacity', 1.5 - ((Math.round((firstTop - pos) * speedFactor))/100));
+                    // specific to bandana
+                    $('#hero img').css('opacity', 1.65 - ((Math.round((firstTop - pos) * speedFactor))/100));
+                }
+                if (fade == 'single') {
+                    $this.css('opacity', 1.3 - ((Math.round((firstTop - pos) * speedFactor))/100));
+                    // specific to bandana
+                }
+            });
+        }       
+        $window.bind('scroll', update).resize(update);
+        update();
+    };
+})(jQuery);
