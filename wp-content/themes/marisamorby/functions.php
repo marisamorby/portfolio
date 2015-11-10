563ed88205b0b7aabfba56c97a86f8d8cf602539
marisamorby <me@marisamorby.com>
2015-11-10 15:54:57 +0700

    Responsive video container embed

--- wp-content/themes/marisamorby/functions.php
+++ wp-content/themes/marisamorby/functions.php
@@ -203,5 +203,16 @@ function hb_register_required_plugins(  ) {
     );
 
     tgmpa($plugins, $config);
+
+
 }
 
+
+/* Video embed wrapper */
+    
+    add_filter( 'embed_oembed_html', 'custom_oembed_filter', 10, 4 ) ;
+
+function custom_oembed_filter($html, $url, $attr, $post_ID) {
+    $return = '<div class="video-container">'.$html.'</div>';
+    return $return;
+}
